803d9076ec2a4fd0f026bb970d9fbee797136de2
Adriano Rodrigues <adrianordp1@gmail.com>
2026-04-04 13:09:34 -0400

    feat(app/models/pokemons.py): add relationship to WildEncounters in Pokemons model

--- app/models/pokemons.py
+++ app/models/pokemons.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+
+if TYPE_CHECKING:
+    from app.models.wild_encounters import WildEncounters
 
 
 class Pokemons(Base):
@@ -12,3 +17,7 @@ class Pokemons(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
 
     name: Mapped[str] = mapped_column(String(255), unique=True)
+
+    wild_encounters: Mapped[list["WildEncounters"]] = relationship(
+        "WildEncounters", back_populates="pokemon"
+    )
