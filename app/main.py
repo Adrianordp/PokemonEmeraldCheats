cfdbe8fbc98ed113a8e31d475941ee2fb53f4450
Adriano Rodrigues <adrianordp1@gmail.com>
2026-04-04 14:19:45 -0400

    refactor(app/main.py): remove redundant cheat retrieval functions and streamline imports

--- app/main.py
+++ app/main.py
@@ -5,65 +5,9 @@ encounters, and wild encounter levels from database.
 
 import json
 
-from app.core.database import get_db
-from app.repository.pokemons import PokemonsRepository
-from app.repository.wild_encounters import (
-    WildEncountersRepository,  # noqa: F401
-)
-from app.repository.wild_encounters_level import WildEncountersLevelRepository
-
-
-def get_pokemon_cheats(pokemon_name: str) -> dict[str, list[str]]:
-    """Get all cheat codes related to a specific pokemon."""
-    db = next(get_db())
-    pokemon = PokemonsRepository.read_by_name(db, pokemon_name.upper())
-
-    if not pokemon:
-        raise ValueError(f"No pokemon found with name '{pokemon_name}'")
-
-    pokemon_data = pokemon.model_dump()
-
-    pokemon_data.pop("id", None)
-    pokemon_data.pop("name", None)
-
-    cheats = {}
-
-    if pokemon_data.get("wild_encounters"):
-        cheats["wild_encounters"] = []
-        for encounter in pokemon_data["wild_encounters"]:
-            if encounter.get("code"):
-                cheats["wild_encounters"].append(encounter["code"])
-
-    return cheats
-
-
-def get_pokemon_encounter_cheat(pokemon_name: str) -> list[str]:
-    """Get the cheat codes for a specific pokemon."""
-    db = next(get_db())
-    pokemon = PokemonsRepository.read_by_name(db, pokemon_name.upper())
-
-    if not pokemon:
-        raise ValueError(f"No pokemon found with name '{pokemon_name}'")
-
-    if not pokemon.wild_encounters:
-        raise ValueError(
-            f"No wild encounters found for pokemon '{pokemon_name}'"
-        )
-
-    return [encounter.code for encounter in pokemon.wild_encounters]
-
-
-def get_level_cheat(level: int) -> str:
-    """Get the cheat code for a specific wild encounter level."""
-    db = next(get_db())
-    wild_encounters_level = WildEncountersLevelRepository.read_by_level(
-        db, level=level
-    )
-
-    if not wild_encounters_level:
-        raise ValueError(f"No cheat found for level {level}")
-
-    return wild_encounters_level.code
+from app.services.get_level_cheat import get_level_cheat
+from app.services.get_pokemon_cheats import get_pokemon_cheats
+from app.services.get_pokemon_encounter_cheat import get_pokemon_encounter_cheat
 
 
 def main():
