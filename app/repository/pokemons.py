8270adbd0ba2f2bbc22da105f7cd92a47fe216d0
Adriano Rodrigues <adrianordp1@gmail.com>
2026-04-04 14:19:28 -0400

    refactor(app/repository/pokemons.py): improve code readability

--- app/repository/pokemons.py
+++ app/repository/pokemons.py
@@ -20,16 +20,19 @@ class PokemonsRepository:
         db.add(db_pokemon)
         db.commit()
         db.refresh(db_pokemon)
+
         return PokemonRead.model_validate(db_pokemon)
 
     @staticmethod
     def read_all(db: Session) -> list[PokemonRead]:
         pokemons = db.query(Pokemons).all()
+
         return [PokemonRead.model_validate(pokemon) for pokemon in pokemons]
 
     @staticmethod
     def read_by_id(db: Session, pokemon_id: int) -> Optional[PokemonReadFull]:
         pokemon = db.query(Pokemons).filter(Pokemons.id == pokemon_id).first()
+
         if pokemon:
             return PokemonReadFull.model_validate(pokemon)
         return None
@@ -37,8 +40,10 @@ class PokemonsRepository:
     @staticmethod
     def read_by_name(db: Session, name: str) -> Optional[PokemonReadFull]:
         pokemon = db.query(Pokemons).filter(Pokemons.name == name).first()
+
         if pokemon:
             return PokemonReadFull.model_validate(pokemon)
+
         return None
 
     @staticmethod
@@ -48,6 +53,7 @@ class PokemonsRepository:
         db_pokemon = (
             db.query(Pokemons).filter(Pokemons.id == pokemon_id).first()
         )
+
         if not db_pokemon:
             return None
 
@@ -58,6 +64,7 @@ class PokemonsRepository:
 
         db.commit()
         db.refresh(db_pokemon)
+
         return PokemonRead.model_validate(db_pokemon)
 
     @staticmethod
@@ -65,8 +72,11 @@ class PokemonsRepository:
         db_pokemon = (
             db.query(Pokemons).filter(Pokemons.id == pokemon_id).first()
         )
+
         if not db_pokemon:
             return False
+
         db.delete(db_pokemon)
         db.commit()
+
         return True
