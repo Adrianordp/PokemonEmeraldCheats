e1d96fe69a2949eea98bef35065b6b2fed0eebec
Adriano Rodrigues <adrianordp1@gmail.com>
2026-04-06 13:52:18 -0400

    refactor(app/services/get_pokemon_cheats.py): add functions to format cheat codes as a string and retrieve them for a specific pokemon

--- app/services/get_pokemon_cheats.py
+++ app/services/get_pokemon_cheats.py
@@ -9,6 +9,26 @@ from app.core.database import get_db
 from app.repository.pokemons import PokemonsRepository
 
 
+def _format_as_string(cheat_codes: dict[str, list[str]]) -> str:
+    """Format the cheat codes as a string."""
+    formatted_codes = ""
+
+    for pokemon_name, codes in cheat_codes.items():
+        formatted_codes += f"{pokemon_name}:\n"
+        for code in codes:
+            formatted_codes += f"  - {code}\n"
+
+    return formatted_codes
+
+
+def get_pokemon_cheats_as_string(pokemon_name: str) -> str:
+    """
+    Get all cheat codes related to a specific pokemon formatted as a string.
+    """
+    cheat_codes = get_pokemon_cheats(pokemon_name)
+    return _format_as_string(cheat_codes)
+
+
 def get_pokemon_cheats(pokemon_name: str) -> dict[str, list[str]]:
     """Get all cheat codes related to a specific pokemon."""
     db = next(get_db())
@@ -41,6 +61,13 @@ def main():
         cheats = get_pokemon_cheats(name)
         print(f"All cheat codes for pokemon '{name}':")
         print(json.dumps(cheats, indent=2))
+    except ValueError as e:
+        print(e)
+
+    try:
+        cheat_codes = get_pokemon_cheats_as_string(name)
+        print(f"All cheat codes for pokemon '{name}' formatted as string:")
+        print(cheat_codes)
     except ValueError as e:
         print(e)
 
